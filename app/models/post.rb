d7a18618dee0356e0922127844f95036229a1052
Farman Pirzada <farmanp.home@gmai.com>
2016-01-26 16:26:38 -0500

    Add published and unpublished functions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :author, class_name: :user, foreign_key: :author_id
+  scope :ordered, :order => "created_at DESC"
+
+  def self.published
+    Post.where(is_published: true)
+  end
+
+  def self.unpublished
+    Post.where(is_published: false)
+  end
 end
