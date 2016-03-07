42d3e5ab96a1d82c1534792e282dc0c4119f0263
Max <maximillini86@gmail.com>
2016-03-07 10:26:57 -0600

    Add views for tags, link to tag show page from post show page

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,3 +1,14 @@
 class TagsController < ApplicationController
+  before_action :get_tag, only: [:show]
+  def index
+  end
 
+  def show   
+  end
+
+  private
+
+  def get_tag
+    @tag = Tag.find(params[:id])
+  end
 end
